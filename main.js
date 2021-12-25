357bb9c6be528a6bdc272995285b506276e359ef
archnim <archnim01@gmail.com>
2021-12-25 11:49:19 +0000

    Inner proxying system rewritten

--- main.js
+++ main.js
@@ -1,38 +1,61 @@
 #! /usr/bin/env node
 
 let fs = require("fs"),
-	httpProxy = require('http-proxy'),
+	http = require("http"),
+	https = require("https"),
 
 	input = process.argv[2],
 	output = process.argv[3],
-	host = process.argv[4],
-	cert = process.argv[5],
-	key = process.argv[6]
+	cert = process.argv[4],
+	key = process.argv[5]
 ;
+
+function serve (client_req, client_res) {
+	var options = {
+		hostname: "localhost",
+		port: input,
+		path: client_req.url,
+		method: client_req.method,
+		headers: client_req.headers
+	};
+	var req = http.request(options, function(res) {
+		client_res.writeHead(res.statusCode, res.headers);
+		res.on('data', function (chunk) {
+			client_res.write(chunk);
+		});
+		res.on('end', function () {
+			client_res.end();
+		});
+	});
+	req.end();
+}
 
 (function prox() {
 	try {
 		let proxy;
-		if(cert)
-			proxy = httpProxy.createServer({
-				target: {
-					host, port: input
-				},
-				ssl: {
+		if(cert) {
+			proxy = https.createServer(
+				{
 					key: fs.readFileSync(key, 'utf8'),
 					cert: fs.readFileSync(cert, 'utf8')
-				}
-			}).listen(output);
-		else
-			proxy = httpProxy.createProxyServer({
-				target:`http://localhost:${input}`
-			}).listen(output);
+				}, serve
+			);
+			
+			proxy.listen(output);
+		}
+		else {
+			proxy = http.createServer(serve);
 
-		proxy.on('error', prox);
+			proxy.listen(output);
+		}
+
+		proxy.on("error", (err) => {
+			console.err(err.message);
+		});
 	}
 	catch(err) {
 		console.log(err.message);
-		//prox();
+		prox();
 	}
 })();
 
