610997172fe62f05444f5ae3d67777ec033feebf
archnim <archnim01@gmail.com>
2021-12-25 08:48:39 +0000

    better https support

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
 #! /usr/bin/env node
-let fs = require("fs"),
-    httpProxy = require('http-proxy'),
 
-    input = process.argv[2],
-    output = process.argv[3],
-    cert = process.argv[4],
-    key = process.argv[5]
+let fs = require("fs"),
+	httpProxy = require('http-proxy'),
+
+	input = process.argv[2],
+	output = process.argv[3],
+	host = process.argv[4],
+	cert = process.argv[5],
+	key = process.argv[6]
 ;
 
 (function prox() {
-    try {
-        if(cert)
-            httpProxy.createServer({
-                target: {
-                    host: 'localhost',
-                    port: input
-                },
-                ssl: {
-                    key: fs.readFileSync(key, 'utf8'),
-                    cert: fs.readFileSync(cert, 'utf8')
-                }
-            }).listen(output);
-        else
-            httpProxy.createProxyServer({
-                target:`http://localhost:${input}`
-            }).listen(output);
-    }
-    catch(err) {
-        prox();
-    }
+	try {
+		let proxy;
+		if(cert)
+			proxy = httpProxy.createServer({
+				target: {
+					host, port: input
+				},
+				ssl: {
+					key: fs.readFileSync(key, 'utf8'),
+					cert: fs.readFileSync(cert, 'utf8')
+				}
+			}).listen(output);
+		else
+			proxy = httpProxy.createProxyServer({
+				target:`http://localhost:${input}`
+			}).listen(output);
+
+		proxy.on('error', prox);
+	}
+	catch(err) {
+		console.log(err.message);
+		//prox();
+	}
 })();
 
 console.log(`Proxying form port: ${process.argv[2]} to port: ${process.argv[3]}...`);
